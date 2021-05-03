0460ed392f83d7a8d4a032dbc6e1478b8e262337
UrlBin <urlbin@yandex.ru>
2021-05-03 19:11:25 +0300

    Прорисовка шахматной доски с помощью модуля pygame. Получилось нарисовать доску, цифры на доске, реализовать обводку клетки при наведении курсором, при нажатии клавиши мыши отрисовывается круг внутри клетки.

--- Chess_horse_drawing.py
+++ Chess_horse_drawing.py
@@ -1,13 +1,104 @@
 import pygame
 
-
 number_of_fealds = 8
 dim_of_rec = 80
+width = dim_of_rec*number_of_fealds + 30
+height = dim_of_rec*number_of_fealds + 30
 
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([dim_of_rec*number_of_fealds, dim_of_rec*number_of_fealds])
+screen = pygame.display.set_mode([width, height])
+pygame.display.set_caption('Демонстрация ходов фигуры КОНЬ на доске')
+clock = pygame.time.Clock()
+FPS = 60
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+
+class myButton:
+
+    circle_bool = False
+
+    def __init__(self, screen, colour, count_x_i, count_y_i, count_x_j, count_y_j):
+
+        self.screen = screen
+        self.colour = colour
+        self.count_x_i = count_x_i
+        self.count_y_i = count_y_i
+        self.count_x_j = count_x_j
+        self.count_y_j = count_y_j
+        self.coordinate = (count_x_i, count_y_i, dim_of_rec, dim_of_rec)
+        self.coordinate_of_center = ((self.count_x_i + self.count_x_j) / 2, (self.count_y_i + self.count_y_j) / 2)
+        self.internal_circle_bool = False
+
+        # print('Создан новый объект класса myButton.')
+        # print('Его координаты: первая точка - ', count_x_i, count_y_i, '; - вторая точка - ', count_x_j, count_y_j)
+
+    def draw_rec(self):
+        # pygame.draw.rect(self.screen, self.colour, self.coordinate)
+        pygame.draw.rect(self.screen, self.colour, self.coordinate)
+        # pygame.draw.circle(self.screen, (0, 255, 0), (self.count_x_i, self.count_y_i), 5)
+        # pygame.draw.circle(self.screen, (0, 0, 255), (self.count_x_j, self.count_y_j), 5)
+
+    def draw_border(self):
+        focused_colour = (255, 0, 0)
+        mouse_active = pygame.mouse.get_pos()
+        # coordinate_of_center = ((self.count_x_i + self.count_x_j)/2, (self.count_y_i + self.count_y_j)/2)
+        if self.count_x_i < mouse_active[0] < self.count_x_j:
+            if self.count_y_i < mouse_active[1] < self.count_y_j:
+                # pygame.draw.rect(self.screen, focused_colour, self.coordinate, 5)
+                pygame.draw.rect(self.screen, focused_colour, self.coordinate, 5)
+
+    def draw_circle(self):
+        # mouse_click = pygame.mouse.get_pressed()
+        mouse_click = pygame.mouse.get_pressed()
+        mouse_active = pygame.mouse.get_pos()
+        # coordinate_of_center = ((self.count_x_i + self.count_x_j)/2, (self.count_y_i + self.count_y_j)/2)
+        if self.count_x_i < mouse_active[0] < self.count_x_j:
+            if self.count_y_i < mouse_active[1] < self.count_y_j:
+                if mouse_click[0] == True or mouse_click[2] == True:
+                    pygame.time.delay(50)
+                    if myButton.circle_bool == False and self.internal_circle_bool == False:
+                        myButton.circle_bool = True
+                        self.internal_circle_bool = True
+                        pygame.time.delay(50)
+                    else:
+                        myButton.circle_bool = False
+                        self.internal_circle_bool = False
+                        pygame.time.delay(50)
+        if self.internal_circle_bool == True:
+            pygame.draw.circle(self.screen, (0, 255, 0), self.coordinate_of_center, 20)
+
+screen.fill((255, 255, 255))
+
+# Draw a solid blue circle in the center
+list_of_buttons = list()
+for i in range(0, number_of_fealds):
+    count_x_i = 15
+    count_y_i = dim_of_rec*i + 15
+    count_x_j = count_x_i + dim_of_rec
+    count_y_j = count_y_i + dim_of_rec
+    if i%2 == 0:
+        for j in range(0, number_of_fealds):
+            if j % 2 == 0:
+                # print(count_x_i, count_y_i, '--', count_x_j, count_y_j)
+                list_of_buttons.append(myButton(screen, black, count_x_i, count_y_i, count_x_j, count_y_j))
+            else:
+                # print(count_x_i, count_y_i, '--', count_x_j, count_y_j)
+                list_of_buttons.append(myButton(screen, white, count_x_i, count_y_i, count_x_j, count_y_j))
+            count_x_i += dim_of_rec
+            count_x_j += dim_of_rec
+    else:
+        for j in range(0, number_of_fealds):
+            if j % 2 == 0:
+                # print(count_x_i, count_y_i, '--', count_x_j, count_y_j)
+                list_of_buttons.append(myButton(screen, white, count_x_i, count_y_i, count_x_j, count_y_j))
+            else:
+                # print(count_x_i, count_y_i, '--', count_x_j, count_y_j)
+                list_of_buttons.append(myButton(screen, black, count_x_i, count_y_i, count_x_j, count_y_j))
+            count_x_i += dim_of_rec
+            count_x_j += dim_of_rec
 
 running = True
 while running:
@@ -18,37 +109,41 @@ while running:
             running = False
 
     # Fill the background with white
-    screen.fill((255, 255, 255))
 
-    # Draw a solid blue circle in the center
-    for i in range(0, number_of_fealds):
-        count_x_i = 0
-        count_y_i = dim_of_rec*i
-        count_x_j = dim_of_rec
-        count_y_j = dim_of_rec*(i + 1)
-        if i%2 == 0:
-            for j in range(0, number_of_fealds):
-                if j % 2 == 0:
-                    pygame.draw.rect(screen, (0, 0, 0), (count_x_i, count_y_i, count_x_j, count_y_j))
-                    count_x_i += dim_of_rec
-                    count_x_j += dim_of_rec
-                else:
-                    pygame.draw.rect(screen, (255, 255, 255), (count_x_i, count_y_i, count_x_j, count_y_j))
-                    count_x_i += dim_of_rec
-                    count_x_j += dim_of_rec
-        else:
-            for j in range(0, number_of_fealds):
-                if j % 2 == 0:
-                    pygame.draw.rect(screen, (255, 255, 255), (count_x_i, count_y_i, count_x_j, count_y_j))
-                    count_x_i += dim_of_rec
-                    count_x_j += dim_of_rec
-                else:
-                    pygame.draw.rect(screen, (0, 0, 0), (count_x_i, count_y_i, count_x_j, count_y_j))
-                    count_x_i += dim_of_rec
-                    count_x_j += dim_of_rec
+    for m in list_of_buttons:
+        m.draw_rec()
+
+    # for n in list_of_buttons:
+    #     n.draw_circle()
+
+    pygame.draw.rect(screen, white, (0, 0, width, height), 30)
+    pygame.draw.rect(screen, black, (15, 15, width - 30, height - 30), 3)
+
+    count_of_text_place_x = dim_of_rec/2 + 15
+    for k in range(0, number_of_fealds):
+        f1 = pygame.font.Font(None, 25)
+        text1 = f1.render('{}'.format(k), True,
+                      (0, 0, 0))
+        screen.blit(text1, (count_of_text_place_x, height - 15))
+        count_of_text_place_x += dim_of_rec
+
+    count_of_text_place_y = height - dim_of_rec / 2 - 15
+    for k in range(0, number_of_fealds):
+        f1 = pygame.font.Font(None, 25)
+        text1 = f1.render('{}'.format(k), True,
+                          (0, 0, 0))
+        screen.blit(text1, (width - 10, count_of_text_place_y))
+        count_of_text_place_y -= dim_of_rec
+
+    for m in list_of_buttons:
+        m.draw_border()
+
+    for n in list_of_buttons:
+        n.draw_circle()
 
     # Flip the display
     pygame.display.flip()
+    clock.tick(FPS)
 
 # Done! Time to quit.
 pygame.quit()
